a005a72885ed780249f6aa65656a2dbabad862eb
Tyaraju Terra <tyarajuterra@hotmail.com>
2023-07-12 17:32:14 -0300

    Added order feature to videos content

--- components/contentVideos.tsx
+++ components/contentVideos.tsx
@@ -24,37 +24,82 @@ const ContentVideos = () => {
     })    
   },[])
 
-  const [paginaAtual, setPaginaAtual] = useState(1);
-  const [categoriaSelecionada, setCategoriaSelecionada] = useState(null);
-  const resultadosPorPagina = 9;
-  const indiceInicial = (paginaAtual - 1) * resultadosPorPagina;
-  const indiceFinal = paginaAtual * resultadosPorPagina;
-  const dadosFiltrados = categoriaSelecionada ? videos.filter((registro) => registro.type === categoriaSelecionada) : videos;
-  const resultadosPaginados = dadosFiltrados.slice(indiceInicial, indiceFinal);
-  const numeroDePaginas = Math.ceil(dadosFiltrados.length / resultadosPorPagina);
-
-  const trocarPagina = (novaPagina) => {
-    setPaginaAtual(novaPagina);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [openSelect, setOpenSelect] = useState(false);
+  const resultsPage = 9;
+  const firstResult = (currentPage - 1) * resultsPage;
+  const finalResult = currentPage * resultsPage;
+  const filterData = selectedCategory ? videos.filter((result) => result.type === selectedCategory) : videos;
+  const orderArray = ['Data de Publicação', 'Nome', 'Duração'];
+  const orderAliasArray = ['publicacao', 'title', 'duration'];
+  const [selectedNameOrder, setSelectedNameOrder] = useState(orderArray[0]);
+  const [selectedAliasOrder, setSelectedAliasOrder] = useState(orderAliasArray[0]);
+  
+  // Ordenar os dados com base na opção de ordenação selecionada
+  const dadosOrdenados = filterData.sort((a, b) => {
+    //if(openSelect){
+      //console.log(selectedAliasOrder);
+      
+      if (selectedAliasOrder === 'publicacao') {
+        return new Date(a.publicacao) - new Date(b.publicacao);
+      } else if (selectedAliasOrder === 'duration') {
+        //console.log(a.duration.localeCompare(b.duration));
+        
+        return a.duration.localeCompare(b.duration);
+      } else if (selectedAliasOrder === 'title') {
+        return a.title.localeCompare(b.title);
+      }
+    //}
+    return 0;
+  });
+  
+  const pageResults = dadosOrdenados.slice(firstResult, finalResult);
+  const totalPages = Math.ceil(dadosOrdenados.length / resultsPage);
+  
+  
+  const changePage = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  const changeOrder = (newOrder) => {    
+    setSelectedAliasOrder(newOrder)
+    setOpenSelect(false)
+    setCurrentPage(1);
   };
 
-  const aplicarFiltroCategoria = (categoria: string) => {
-    if(categoriaSelecionada === categoria) {
-      setCategoriaSelecionada(null);
+  const setOrderSelect = () => {
+    const orderSelect = orderAliasArray.map((order,index) => (
+      <div 
+        className="flex justify-between items-center my-2 px-[8px] py-[5px]" 
+        key={index} 
+        onClick={() => { 
+          changeOrder(order); 
+          setSelectedNameOrder(orderArray[index]);
+        }}
+      >
+        <p className="mb-0">{orderArray[index]}</p>
+      </div>
+    ));
+    return orderSelect;
+  }
+  const setCategories = (category: string) => {
+    if(selectedCategory === category) {
+      setSelectedCategory(null);
     } else {
-      setCategoriaSelecionada(categoria);
+      setSelectedCategory(category);
     }
-    setPaginaAtual(1);
+    setCurrentPage(1);
   };
 
-  const gerarBotoesPagina = () => {
-    const botoes = [];
-    for (let i = 1; i <= numeroDePaginas; i++) {
-      botoes.push(
-        <div className="buttonPages">
+  const setButtonsPage = () => {
+    const buttonsPage = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttonsPage.push(
+        <div className="button_pages">
           <button
             key={i}
-            onClick={() => trocarPagina(i)}
-            disabled={i === paginaAtual}
+            onClick={() => changePage(i)}
+            disabled={i === currentPage}
             className='font-JakartaSansMedium text-copy-default justify-self-end text-[24px] px-[14px] py-[3px]'
           >
             {i}
@@ -62,19 +107,19 @@ const ContentVideos = () => {
         </div>
       );
     }
-    return botoes;
+    return buttonsPage;
   };
 
-  const gerarBotoesCategoria = () => {
-    const categorias = ['Agências', 'Chatbot', 'Marketing Digital', 'Geração de Leads', 'Mídia Paga']; 
-    const botoes = categorias.map((categoria) => (
+  const setCategoriesPage = () => {
+    const categories = ['Agências', 'Chatbot', 'Marketing Digital', 'Geração de Leads', 'Mídia Paga']; 
+    const botoes = categories.map((category) => (
       <button
         type='button'
-        className={`button_videos ${(categoria === categoriaSelecionada) ? ' active' : ''}`}
-        key={categoria}
-        onClick={() => aplicarFiltroCategoria(categoria)}
+        className={`button_videos ${(category === selectedCategory) ? ' active' : ''}`}
+        key={category}
+        onClick={() => setCategories(category)}
       >
-        {categoria}
+        {category}
       </button>
     ));
     return botoes;
@@ -88,26 +133,25 @@ const ContentVideos = () => {
           <div className='menuVideos'>
             <div className="flex flex-row">
               <div className="grid-cols-8 basis-2/3 flex justify-start">
-                {gerarBotoesCategoria()}
+                {setCategoriesPage()}
               </div>
-              <div className="grid-cols-4 basis-1/3 flex flex-row justify-end">
-                <span className='font-JakartaSansExtraBold py-[6px] justify-self-end text-[15px] pr-[14px]'>Ordernar por</span>
-                <div className="buttonSelect border divide-solid border-[#2c3e50] rounded-xl px-[8px] py-[5px]">
-                  <div className="">
-                    <div className="flex justify-between items-center">
-                      <h3 className="mr-[4px]">Data de Publicação</h3>
-                      <svg stroke="currentColor" fill="currentColor" viewBox="0 0 320 512" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path>
-                      </svg>
+              <div className="grid-cols-4 basis-1/3 flex flex-row justify-end ">
+                <div className="flex flex-row justify-end">
+                  <span className='font-JakartaSansExtraBold py-[6px] justify-self-start text-[15px] pr-[14px]'>Ordernar por</span>
+                  <div className="min-w-[190px] ">
+                    <div className="buttonSelect border divide-solid border-[#2c3e50] rounded-xl px-[8px] py-[5px]">
+                      <div className="flex justify-between items-center">
+                        <h3 className="mr-[4px]" id='selectOrder' onClick={() => {setOpenSelect(true)}}>{selectedNameOrder}</h3>
+                        <svg stroke="currentColor" fill="currentColor" viewBox="0 0 320 512" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path>
+                        </svg>
+                      </div>
                     </div>
-                  </div>
-                  <div className='hidden'>
-                    <div className="_style_selector__5Dlrk flex justify-between items-center my-2">
-                      <p className="f_size_18 mb-0">Data de Publicação</p>
+                    {openSelect && (
+                    <div className='absolute border divide-solid border-[#2c3e50] rounded-xl px-[8px] py-[5px] bg-white'>
+                      {setOrderSelect()}
                     </div>
-                    <div className="_style_selector__5Dlrk flex justify-between items-center my-2">
-                      <p className="f_size_18 mb-0">Nome</p>
-                    </div>
+                    )}
                   </div>
                 </div>
               </div>
@@ -120,9 +164,9 @@ const ContentVideos = () => {
       </div>
       <div className="mx-auto max-w-6xl py-lg-5 px-4 sm:px-5 lg:px-1.5 pb-16 text-center">
         <div className="flex flex-row flex-wrap justify-between gap-x-4 gap-y-7 grid grid-cols-3">
-          {resultadosPaginados.map((resultado, index) => {
+          {pageResults.map((resultado, index) => {
             return  <div className="box_video md:max-w-[360px]" key={index}>
-                      <div className="thumb_video">
+                      <div className="thumb_video rounded-t-[20px] overflow-hidden">
                         <Image
                           src={`/videos/thumbs/${resultado.thumb}`}
                           alt={resultado.title}
@@ -140,11 +184,11 @@ const ContentVideos = () => {
         <div className="col-xl-12 col-lg-12">
           <hr className="h-0.5 mt-[76px] mb-[28px] bg-[#D5D9DD] border-0 dark:bg-gray-700" />
         </div>
-        { numeroDePaginas > 1 && ( 
+        { totalPages > 1 && ( 
         <div className="col-xl-12 col-lg-12">
           <div className="col-xl-12 col-lg-12 flex flex-row justify-center py-[5px] ">
             <span className='font-JakartaSansBold text-copy-default leading-10 justify-self-end text-[24px] px-[14px]'>Página</span>
-            {gerarBotoesPagina()}
+            {setButtonsPage()}
           </div>
         </div>
         ) }
